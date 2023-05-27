223b865c30852d402557053ae6530e896cc5495d
matibf99 <matibf99@gmail.com>
2023-05-27 18:37:43 -0300

    added more attributes to User.java

--- src/main/java/com/example/microserviceuar/domain/User.java
+++ src/main/java/com/example/microserviceuar/domain/User.java
@@ -3,6 +3,8 @@ package com.example.microserviceuar.domain;
 import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -11,6 +13,9 @@ public class User {
 
     @Id
     private UUID id;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String username;
+    private String password;
     private String firstName;
     private String lastName;
     private String nationality;
