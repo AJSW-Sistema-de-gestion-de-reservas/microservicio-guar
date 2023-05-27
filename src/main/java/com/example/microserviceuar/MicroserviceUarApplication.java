cdeac7857b2ce184cac42b192236a4a28c2436fc
matibf99 <matibf99@gmail.com>
2023-05-27 18:24:00 -0300

    add project files

---
+++ src/main/java/com/example/microserviceuar/MicroserviceUarApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceuar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microserviceuar/MicroserviceUarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceuar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
